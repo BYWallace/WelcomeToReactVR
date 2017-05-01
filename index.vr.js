664995a18ce2db825e3f38ef4aadfe623412baef
Brett Wallace <bwallace90@gmail.com>
2017-04-30 21:08:04 -0700

    Add colored buttons w/ change on hover for first one

--- index.vr.js
+++ index.vr.js
@@ -9,27 +9,52 @@ import {
 } from 'react-vr';
 
 export default class WelcomeToReactVR extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {backgroundColor: 'red'};
+  }
+
   render() {
-    return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
+  // <View> below creates a view that is 2 meters wide and is positioned
+  // 5 meters in front of the user (z = -5). Its child items are laid out
+  // in a 'column' and marked to 'stretch' to the width of the view container.
+  // This causes call child view to have the same width.
+  return (
+    <View>
+      <Pano source={asset('chess-world.jpg')} />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        width: 2,
+        alignItems: 'stretch',
+        transform: [{translate: [-1, 1, -6]}, {rotateX: -20}],
+      }}>
+
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: this.state.backgroundColor}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}
+          onEnter={() => this.setState({backgroundColor: 'blue'})}
+          onExit={() => this.setState({backgroundColor: 'red'})}>
+          Red
         </Text>
       </View>
-    );
-  }
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
+      </View>
+      <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
+        <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
+      </View>
+
+    </View>
+    </View>
+  );
+}
 };
 
 AppRegistry.registerComponent('WelcomeToReactVR', () => WelcomeToReactVR);
